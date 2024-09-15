b3716c8485522c76731ef9bf2100a694d611aa3f
tbardhan <tirthankar.bardhan@ibm.com>
2024-09-15 22:32:13 +0530

    added serviceaccount and read root system

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -17,7 +17,7 @@ scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https:/
     # Kubesec scan result processing
     # echo "Scan Score : $scan_score"
 
-	if [[ "${scan_score}" -ge 0 ]]; then
+	if [[ "${scan_score}" -ge 5 ]]; then
 	    echo "Score is $scan_score"
 	    echo "Kubesec Scan $scan_message"
 	else
