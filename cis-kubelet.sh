a8521e2ebcb70130b2e7e8afdb83063727a04d20
tbardhan <tirthankar.bardhan@ibm.com>
2024-09-23 22:42:41 +0530

    Adding Kube bench & CIS check to jenkins pipeline

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #cis-kubelet.sh
 
-total_fail=$(kube-bench run --targets node  --version 1.15 --check 2.1.2,2.1.3 --json | jq .[].total_fail)
+total_fail=$(kube-bench run --targets node  --version 1.15 --check 2.1.10,2.1.11 --json | jq .[].total_fail)
 
 if [[ "$total_fail" -ne 0 ]];
         then
-                echo "CIS Benchmark Failed Kubelet while testing for 2.1.2, 2.1.3"
+                echo "CIS Benchmark Failed Kubelet while testing for 2.1.10, 2.1.11"
                 exit 1;
         else
-                echo "CIS Benchmark Passed Kubelet for 2.1.2, 2.1.3"
+                echo "CIS Benchmark Passed Kubelet for 2.1.10, 2.1.11"
 fi;
